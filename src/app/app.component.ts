d9ba5b47769c96a9cd3158553b4aaa98fa4a9923
Robert Alonso <robert.alonso.ac@gmail.com>
2017-05-11 22:38:51 -0500

    theme customization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Optional } from '@angular/core';
+import { MdDialog, MdDialogRef, MdSnackBar } from '@angular/material';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+	spaceScreens: Array<any>;
+
+	constructor(private http:Http) {
+		this.http.get('./assets/data.json')
+			.map(response => response.json().screenshots)
+			.subscribe(res => this.spaceScreens = res);
+	}
+
+	likeMe(i) {
+		if (this.spaceScreens[i].liked == 0)
+			this.spaceScreens[i].liked = 1;
+		else
+			this.spaceScreens[i].liked = 0;
+	}
+
+	deleteMe(i) {
+		this.spaceScreens.splice(i, 1);
+		console.log(i);
+	}
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
+import 'hammerjs';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    BrowserAnimationsModule,
   ],
   providers: [],
+  entryComponents: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
